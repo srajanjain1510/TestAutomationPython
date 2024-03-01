8e104e5089946f84bfa9b2061b3cef5f27d6d056
srajan jain <srajan.jain@macys.com>
2024-03-02 02:39:41 +0530

    docker implementation

--- UIAutomation/test_selenium_docker.py
+++ UIAutomation/test_selenium_docker.py
@@ -1,17 +1,11 @@
+import re
+
 from selenium import webdriver
 
 
-
-
 def test_ui_with_docker_image():
-
     options = webdriver.ChromeOptions()
-    # options.add_argument('--ignore-ssl-errors=yes')
-    # options.add_argument('--ignore-certificate-errors')
-    options.set_capability("platformName",'linux')
-    options.set_capability("browserVersion", "122")
-    hub_url = "http://localhost:4444/wd/hub"
+    hub_url = "http://localhost:4444"
     driver = webdriver.Remote(command_executor=hub_url, options=options)
     print(driver.session_id)
-
     driver.get("http://www.browserstack.com/")
